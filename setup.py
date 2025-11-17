1bd93dad79a7bdc1845759968c3513c881b78193
Ehsan Amid <eamid@google.com>
2025-11-17 10:41:26 -0800

    fix __init__

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 configuration = {
     'name' : 'trimap',
-    'version': '1.1.4',
+    'version': '1.1.5',
     'description' : 'TriMap: Large-scale Dimensionality Reduction Using Triplets',
     'long_description' : readme(),
     'classifiers' : [
